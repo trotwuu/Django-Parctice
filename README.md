193ede04207e5db0756abccb781d9717a602eb80
Troy Wu <troy0602@gmail.com>
2021-01-07 16:45:22 +0800

    First commit

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
 # Django-Parctice
+
+### Run Server
+```sh
+$ docker-compose up
+```
+
+```sh
+$ docker-compose up -d
+```
+
+### clost server
+$ docker-compose down
+
+### Django
+If you want to `makemigrations` or `migrate`
+First we have to activate the Django_env
+
+```sh
+$ source ./Django_env/bin/activate
+```
+
+```sh
+$ python3 manage.py makemigrations
+$ python3 manage.py migrate
+```
+
+###
+
+開啟 docker 後進去 docker 的 terminal 的方法
+docker exec -it django-container bash
+為了open python shell 要用的
+還有 python manage.py createsuperuser
---
+++ polls/views.py
@@ -0,0 +1,65 @@
+from django.shortcuts import get_object_or_404,render
+from .models import Question, Choice
+from django.template import loader
+from django.http import HttpResponse, HttpResponseRedirect, Http404
+from django.urls import reverse
+from django.views import generic
+
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by('-pub_date')[:5]
+
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
+
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
+
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
+
+def results(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question': question})
+
+def vote(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        results = request.POST.getlist('choice')
+        selected_choice = []
+        for id in results:
+            selected_choice.append(question.choice_set.get(pk=id))
+        print(selected_choice)
+
+        
+    except (KeyError, Choice.DoesNotExist):
+        # Redisplay the question voting form.
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        for i in selected_choice:
+            i.votes += 1
+            i.save()
+
+        print(selected_choice, results)
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button.
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
+
+def index(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
